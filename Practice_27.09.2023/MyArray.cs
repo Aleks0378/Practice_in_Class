64ffae712d766a42d5bbcecdad4ac4bf005ce6ce
Aleks0378 <aleksstepan0378@gmail.com>
2023-09-27 20:33:00 +0300

    Добавлена практика за 27.09.2023

---
+++ Practice_27.09.2023/MyArray.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+//1) Створити класс MyArray
+//Поле властивість(get, set) size - int: розмір масива
+//Поле - властивість массив int[] arr
+//Конструктор, приймає size, виділяє місце у пам'яті під масив arr;
+//Перевантажити оператор + (MyArray+MyArray повертаємо MyArray)
+//Переробити клас на Узагальнення <T>
+//struct
+namespace Practice_27._09._2023
+{
+    internal class MyArray<T> where T : struct
+    {
+        public int size {get; set;}
+        public T[] Arr { get;}
+        public MyArray (int s)
+        {
+            size = s;
+            if (s!=0)
+            {
+                Arr = new T[s];
+            }
+            else 
+                throw new ArgumentException("Error: The array size can not be zero");
+        }
+        public static MyArray<T> operator +(MyArray<T> a, MyArray<T> b)
+        {
+            if (a.Arr.Length == b.Arr.Length)
+            {
+                MyArray<T> c = new MyArray<T>(a.Arr.Length);
+                for (int i = 0; i < a.Arr.Length; i++)
+                    c.Arr[i] = (dynamic)a.Arr[i] + (dynamic)b.Arr[i];
+                return c;
+            }
+            throw new ArgumentException("The arrays have different sizes");
+        }
+        public int this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < Arr.Length)
+                    return (dynamic)Arr[index];
+                throw new ArgumentException("Invalid Index");
+            }
+            set
+            {
+                if (index >= 0 && index < Arr.Length)
+                    Arr[index] = (dynamic)value;
+                else
+                    throw new ArgumentException("Invalid Index");
+            }
+        }
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                sb.Append(this[i]);
+                if (i < Arr.Length - 1)
+                    sb.Append(',');
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ Practice_27.09.2023/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//2) У Main створити объект, проініціалізувати масив
+//використати оператор +
+namespace Practice_27._09._2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyArray<int> arr1 = new MyArray<int>(6);
+            Random rnd = new Random();
+            for (int i = 0; i < arr1.size; i++)
+            {
+                arr1[i] = rnd.Next(0,50);
+            }
+            Random rnd1 = new Random();
+            MyArray<int> arr2 = new MyArray<int>(6);
+            for (int i = 0; i < arr2.size; i++)
+            {
+                arr2[i] = rnd1.Next(0,50);
+            }
+            MyArray<int> arr3 = new MyArray<int>(6);
+            arr3 = arr1 + arr2;
+            Console.WriteLine("Arr1 = " + arr1);
+            Console.WriteLine("Arr2 = " + arr2);
+            Console.WriteLine("Arr3 = " + arr3);
+        }
+    }
+}
